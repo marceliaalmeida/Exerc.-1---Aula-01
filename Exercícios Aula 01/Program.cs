5b7e5b44832ac4f680ba2f64ffb2ab83fc9f155e
marce <marce@DESKTOP-TRK407I>
2023-07-17 14:01:38 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios Aula 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios_Aula_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { /* Exercício 1: Escreva um programa que faça a leitura do nome do usuário e imprima uma saudação no
+console usando o nome digitado.*/
+            String nome = "";
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("O nome digitado foi: ");
+            Console.WriteLine(nome);
+
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
